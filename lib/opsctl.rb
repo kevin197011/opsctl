027dc1c257b6e908415415d34496e074d5221cfe
kevin <kevin197011.l@gmail.com>
2023-07-10 18:17:16 +0800

    init.

---
+++ lib/opsctl.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "opsctl/version"
+
+module Opsctl
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/opsctl/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Opsctl
+  VERSION = "0.1.0"
+end
